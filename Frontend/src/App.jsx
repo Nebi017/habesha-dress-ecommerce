6d6b7f3aab7e8ac18f5b93df348e8f5ed0355a1b
Tenketem Zeradawit <titihadas123@gmail.com>
2025-04-02 23:51:08 +0300

    yes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
     <Navbar/>
     
     <Routes>
-<Route path="/home" element={<Home/>}/>
+<Route path="/" element={<Home/>}/>
 <Route path='/collection' element={< Collection/>}/>
 <Route path='/about' element={<About/>}/>
 <Route path='/contact' element={<Contact/>}/>
